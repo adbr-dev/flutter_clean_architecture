5876d704bc55336469a54b90e05c688a1c55ef77
bora <bora@artlab.ai>
2023-02-15 15:41:46 +0900

    feat: add utilColors (Theme: primarySwatch)

---
+++ lib/presentation/utils/util_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UtilColors {
+  UtilColors._();
+
+  static const MaterialColor main = MaterialColor(
+    _mainPrimaryValue,
+    <int, Color>{
+      50: Color(_mainPrimaryValue),
+      100: Color(_mainPrimaryValue),
+      200: Color(_mainPrimaryValue),
+      500: Color(_mainPrimaryValue),
+      600: Color(_mainPrimaryValue),
+      700: Color(_mainPrimaryValue),
+    },
+  );
+  static const int _mainPrimaryValue = 0xFF0265e6;
+}
