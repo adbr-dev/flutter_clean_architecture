4ac7618c705b438d0b0823fd0e238a610c5db7e3
bora <bora@artlab.ai>
2023-02-15 16:16:13 +0900

    feat: add UI structure to SearchScreen

--- lib/presentation/features/search/search_screen.dart
+++ lib/presentation/features/search/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/presentation/utils/util_size.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -8,6 +9,41 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('search'),
+      ),
+      body: Column(
+        children: [
+          Form(
+            child: TextFormField(),
+          ),
+          const SizedBox(
+            height: UtilSize.spaceHorizontal,
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(
+                horizontal: UtilSize.paddingHorizontal,
+              ),
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    leading: Image.network(
+                      'https://blog.logrocket.com/wp-content/uploads/2021/04/Building-Flutter-desktop-app-tutorial-examples.png',
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    ),
+                    title: const Text('sitename'),
+                    trailing: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.star_border),
+                    ),
+                  ),
+                );
+              },
+              itemCount: 100,
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/presentation/utils/util_size.dart
@@ -0,0 +1,6 @@
+class UtilSize {
+  UtilSize._();
+
+  static const spaceHorizontal = 20.0;
+  static const paddingHorizontal = 10.0;
+}
