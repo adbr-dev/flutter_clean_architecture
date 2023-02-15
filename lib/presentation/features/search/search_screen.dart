ffbc8fb6d684526d89e0b86f1f14b9172cd1abd4
bora <bora@artlab.ai>
2023-02-15 16:30:52 +0900

    feat: add TextFormField decoration style

--- lib/presentation/features/search/search_screen.dart
+++ lib/presentation/features/search/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/presentation/utils/util_colors.dart';
 import 'package:flutter_clean_architecture/presentation/utils/util_size.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -6,44 +7,89 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('search'),
-      ),
-      body: Column(
-        children: [
-          Form(
-            child: TextFormField(),
-          ),
-          const SizedBox(
-            height: UtilSize.spaceHorizontal,
-          ),
-          Expanded(
-            child: ListView.builder(
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('search'),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: UtilSize.spaceHorizontal,
+            ),
+            Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: UtilSize.paddingHorizontal,
               ),
-              itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    leading: Image.network(
-                      'https://blog.logrocket.com/wp-content/uploads/2021/04/Building-Flutter-desktop-app-tutorial-examples.png',
-                      width: 50,
-                      height: 50,
-                      fit: BoxFit.cover,
-                    ),
-                    title: const Text('sitename'),
-                    trailing: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.star_border),
-                    ),
-                  ),
-                );
-              },
-              itemCount: 100,
+              child: Form(
+                child: TextFormField(
+                  autofocus: true,
+                  decoration: _inputDecoration,
+                  cursorHeight: 22.0,
+                  textInputAction: TextInputAction.search,
+                  onChanged: (value) {},
+                ),
+              ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: UtilSize.spaceHorizontal,
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: UtilSize.paddingHorizontal,
+                ),
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: Image.network(
+                        'https://blog.logrocket.com/wp-content/uploads/2021/04/Building-Flutter-desktop-app-tutorial-examples.png',
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                      ),
+                      title: const Text('sitename'),
+                      trailing: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.star_border),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: 100,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration get _inputDecoration {
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(
+        UtilSize.borderRadius,
+      ),
+      borderSide: BorderSide.none,
+    );
+
+    return InputDecoration(
+      filled: true,
+      fillColor: Colors.white,
+      hintText: '검색할 키워드를 입력해주세요.',
+      enabledBorder: border,
+      focusedBorder: border.copyWith(
+        borderSide: const BorderSide(color: UtilColors.main),
+      ),
+      errorBorder: border.copyWith(
+        borderSide: const BorderSide(color: Colors.red),
+      ),
+      border: border,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+      prefixIcon: const Icon(
+        Icons.search,
+        size: 16,
       ),
     );
   }
--- lib/presentation/utils/util_size.dart
+++ lib/presentation/utils/util_size.dart
@@ -3,4 +3,5 @@ class UtilSize {
 
   static const spaceHorizontal = 20.0;
   static const paddingHorizontal = 10.0;
+  static const borderRadius = 16.0;
 }
