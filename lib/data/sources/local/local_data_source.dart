9de055fbe58bbdf384c741241a1db1a7b8fe3e35
bora <adbr.dev@gmail.com>
2023-02-17 14:35:02 +0900

    feat: remove unused ChangeNotifier in LocalDataSource

--- lib/data/sources/local/local_data_source.dart
+++ lib/data/sources/local/local_data_source.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LocalDataSource with ChangeNotifier {
+class LocalDataSource {
   late SharedPreferences _sharedPreference;
 
   final keyBookmarkUrls = 'bookmark_urls';
@@ -12,7 +11,6 @@ class LocalDataSource with ChangeNotifier {
 
   set bookmarkUrls(List<String> value) {
     _sharedPreference.setStringList(keyBookmarkUrls, value);
-    notifyListeners();
   }
 
   List<String> get bookmarkUrls =>
